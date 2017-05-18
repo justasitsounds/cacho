9f25828413b19baa427980546aec1c26517f627c
justasitsounds <james.prendergast@gmail.com>
2017-05-18 20:18:23 +1000

    Linting engine stuff

--- engine/engine.go
+++ engine/engine.go
@@ -6,11 +6,13 @@ import (
 	"time"
 )
 
+// Dice are the group of one or more Die's
 type Dice struct {
 	randSeed int64
 	dice     []int
 }
 
+//NewDice returns a new set of Dice, with (count) number of Die's
 func NewDice(count int) *Dice {
 	return &Dice{
 		randSeed: time.Now().UnixNano(),
@@ -18,26 +20,27 @@ func NewDice(count int) *Dice {
 	}
 }
 
+// SetSeed sets the RNG seed that determines how the bones fall
 func (d *Dice) SetSeed(seed int64) {
 	d.randSeed = seed
 }
 
+// Roll makes them bones roll
 func (d *Dice) Roll() {
 	r := rand.New(rand.NewSource(d.randSeed))
-	for i, _ := range d.dice {
+	for i := range d.dice {
 		d.dice[i] = r.Intn(5) + 1
 	}
 }
 
+//Score the Dice
 func (d *Dice) Score() []int {
 	sort.Sort(sort.Reverse(sort.IntSlice(d.dice)))
 	return d.dice
 }
 
+//Player No prizes
 type Player struct {
 	dice Dice
-}
-
-func (p *Player) RollDice() {
-
+	name string
 }
